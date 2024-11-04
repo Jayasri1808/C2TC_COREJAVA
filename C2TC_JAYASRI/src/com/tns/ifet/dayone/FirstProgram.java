85962f02d146d7540ebb91c9a50ffbfe793d8faf
JAIGANESH <JAIGANESH@Jayasri>
2024-11-04 14:27:49 +0530

    initial commit

---
+++ C2TC_JAYASRI/src/com/tns/ifet/dayone/FirstProgram.java
@@ -0,0 +1,14 @@
+//Program to demonstrate identifiers  
+package com.tns.ifet.dayone; 
+ 
+public class FirstProgram { 
+ 
+ public static void main(String[] args) { 
+  int $number = 20; 
+  System.out.println("value of the number variable is : "+ $number); 
+  String studentName="Aniket"; 
+  System.out.println("value is : "+ studentName); 
+ }  
+} 
+ 
+ 
