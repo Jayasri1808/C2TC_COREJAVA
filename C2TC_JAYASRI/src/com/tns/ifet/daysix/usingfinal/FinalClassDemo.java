9bfd7642e6a320b06aaba7fe7cc263f385b7988a
Jayasri <JAIGANESH@Jayasri>
2024-11-06 23:32:10 +0530

    initial commit

---
+++ C2TC_JAYASRI/src/com/tns/ifet/daysix/usingfinal/FinalClassDemo.java
@@ -0,0 +1,20 @@
+package com.tns.ifet.daysix.usingfinal;
+
+//final class FinalClass {
+//	void show() {
+//		System.out.println("Final class cannot be inherited");
+//	}
+//}
+class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+class FinalChildClass extends FinalClass {
+}
+public class FinalClassDemo {
+	public static void main(String[] args) {
+		FinalClass f1 = new FinalClass(); 
+		f1.show();
+	}
+}
