7b69222dc5baddb9d601f54b20fc9b48f5656190
JAIGANESH <JAIGANESH@wpad.npavglobal.net>
2024-11-07 23:04:01 +0530

    initial commit

---
+++ C2TC_JAYASRI/src/com/tns/ifet/daytwo/While.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daytwo;
+
+public class While {
+
+	public static void main(String[] args) {
+		int i=99; 
+		  while(i<=100) 
+		  { 
+		   System.out.println(i); 
+		   i++; 
+		  } 
+
+	}
+
+}
