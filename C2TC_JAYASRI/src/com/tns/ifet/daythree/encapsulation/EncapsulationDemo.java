ed068f7a9582b81138738a65cf385271387e5426
JAIGANESH <JAIGANESH@Jayasri>
2024-11-07 22:37:27 +0530

    initial commit

---
+++ C2TC_JAYASRI/src/com/tns/ifet/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.daythree.encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {  
+		OopsConceptDemo obj = new 
+		OopsConceptDemo(); 
+		obj.setSerialNum(123); 
+		obj.setName("Jai"); 
+		obj.setAge(21); 
+		System.out.println(obj); 
+		}
+}
